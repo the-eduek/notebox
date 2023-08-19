8f2b2308540ab9b746f5ca1a195939c612d9bce3
eduek <eduekakpannyah@gmail.com>
2023-08-19 18:29:26 +0100

    fix: use hash router

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@ import './styles/index.css';
 import App from './App.tsx';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { NoteProvider } from './context/NoteContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <NoteProvider>
         <App />
       </NoteProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
