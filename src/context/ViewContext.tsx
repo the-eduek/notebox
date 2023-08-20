eec7f605fb1506924863357fe111016e93595fdf
eduek <eduekakpannyah@gmail.com>
2023-08-20 15:23:16 +0100

    feat: save default view to local storage

--- src/context/ViewContext.tsx
+++ src/context/ViewContext.tsx
@@ -14,9 +14,16 @@ interface ViewProviderProps {
 }
 
 export const ViewProvider: React.FC<ViewProviderProps> = ({ children }: ViewProviderProps) => {
-  const [ notesView, setNotesView ] = useState<ViewType>('list');
+  let localView: ViewType;
+  const isLocal: string | null = localStorage.getItem("localView");
+
+  if (!isLocal) localView = "list";
+  else localView = isLocal as ViewType;
+
+  const [ notesView, setNotesView ] = useState<ViewType>(localView);
 
   const updateView = (newView: ViewType): void => {
+    localStorage.setItem("localView", newView);
     setNotesView(newView);
   };
 
